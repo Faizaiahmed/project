92e59b26b166a92597da0833ba1023b723afc7ed
Faizan Ahmed <76113307+Faizaiahmed@users.noreply.github.com>
2021-01-28 09:22:35 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,198 +1,22 @@
-// Chapter No one!!!!
-
-
-// 1)
-
-//   alert("Welcome to dream world")
-
-// 2)
-
-// alert("javaScript Alert \n Error! Please enter a valid password") 
-
-// 3)
-
-//  alert("javaScript Alert! \n\n Welcome to JS Land....\n Happy Coding!!")
-
-// 4)
-
-//   alert("Welcome To JS Land...")
-
-//  alert("Happly Coding!")
-
-// 5) 
-
-//  console.log("javaScript Alert!! \n \n Hello....I can run JS through my web browser's console.")
-
-                  /* <b> 6 and 7 did not understand please </b> */
-
-
- 
-                  
-                 
-// Chapter No Two!!!!!  
-
- 
-// 2)
-
-// var a = "Faizan";
-// var b = "Ahmed";
-// alert(a + b);
-
-// 3)
-// var i = "Message";
-// var a = "Hello World";
-// alert(i + a );
-
-// 4) 
-
-// var a = prompt("Enter Your Name", "Your Name is");
-// var b = prompt ("Enter Your Age", "Your Age is");
-// var c = prompt("Enter Your Certification", "Your Certification is");
-// var d = "Congratulation Now You Are Enrolled";
-// alert (d);
-
-// 6) 
-// var a = "My Email Address is....\n"
-// var b = ("faizankhi44@gmail.com");
-// var c =(a+b);
-// alert(c);
-
-
-// 7)
-// var a = "Book \n";
-// var b = "I am trying to learn from the Book A Smarter\n Way to learn javaScript"
-// alert(a+b);
-
-
-// 8)
-// document.write("Yah!! I can write HTML content through JavaScript:");
-
-
-
-
-
-// CHAPTER NO THREE!!!
-
-// 1)
-// alert("I am 22 Years Old");
-
-
-// 2)
-// document.write("You have visited 20 times ")
-
-
-// 3)
-// document.write("My Birth Year Is 1999 <br>", "Data type is decleard variable is number");
-
-
-// 4)
-// document.write("<b> Jhone Doe </b> ordered <b> 5 T Shirt </b>(s)on www.XYZ.Clothing.com")
-
-
-
-
-// CHAPTER NO FIVE!!!
-
-
-// 1)
-// var a = 3;
-// var b = 5;
-// document.write(a + b);
-
-
-// 2)
-// var a = 3;
-// var b = 5;
-// document.write(a - b);
-
-
-// var a = 3;
-// var b = 5;
-// document.write(a / b);
-
-// var a = 3;
-// var b = 5;
-// document.write(a * b);
-
-// var a = 3;
-// var b = 5;
-// document.write(a % b)
-
-
-// 4)
-// var a = 600;
-// var b = 5;
-// document.write("The cost of bying 5 tickets is ", a * 5);
-
-// 7)
-// var a = 650;
-// var b = 3;
-// var c = (a * b)
-// document.write(c);
-
-// var a = 100;
-// var b = 7;
-// var c = (a * b);
-// document.write(c);
-
-// var a = 1950
-// var b = 700
-// var c = 100
-// var d =( a + b +c)
-// document.write(d);
-
-
-
-// 8)
-// var a = 980;
-// var b = 804;
-// var c = (b / a * 100);
-// document.write(c);
-
-
-
-// 9)
-// var a = 104.80;
-// var b = 10;
-// var c = (a * b);
-// document.write(c);
-
-
-// var a = 28;
-// var b = 25;
-// var c =(a * b);
-// document.write(c)
-
-
-// var a = 1048;
-// var b = 700;
-// var d = ("Total currency of pkr is ", a + b);
-// document.write(d);
-
-// 10)
-// var a = 5;
-// var b = 10;
-// var c = (a * b);
-// document.write(c);
-
-// var a = 50;
-// var b = 2;
-// var c =(a / 2);
-// document.write(c);
-
-
-// var a = "Current Year is 2016 <br>";
-// var b = "Birth Year is 1992 <br>";
-// var c = "Your age is 24";
-// document.write( a + b +c);
-
-
-// 13)
-// var a = "Favourate Snack : Chocolate Chip <br>";
-// var b = "Current age : 15 <br>";
-// var c = "Estimated Maximum Age : 65 <br>";
-// var d = "Amount of Snack per day : 3 <br>";
-// var f = "You will need 150 Chocolate chip to last you until the ripe old age 65";
-// document.write(a+b+c+d+f);
-
-
+// FOR OVERALL VALUES
+
+function getValue (i){
+    var b = document.getElementById("result")
+    b.value += i
+}
+
+// FOR = VALUE
+
+function getnum(){
+    var b = document.getElementById("result")
+    var val = b.value
+    var final = eval(val)
+    b.value = final
+}
+
+// FOR CLEARVALUE
+
+function clearValue (){
+    var b = document.getElementById("result")
+    b.value = ""
+}
